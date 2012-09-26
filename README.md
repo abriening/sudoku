45ac4dedffd52efbc22a3ef5f1138573c6367e77
Andrew Briening <abriening@gmail.com>
2012-09-25 23:35:16 -0400

    Initial commit - sudoku solver

---
+++ README.md
@@ -0,0 +1,29 @@
+Sudoku Solver
+=============
+
+Just for fun. Read in a json file with 81 numbers, will solve and print the solution.
+
+Usage
+-----
+
+In irb:
+
+    require 'lib/sudoku.rb'
+    game = Sudoku::Game.new
+    game.load_gamefile 'test/fixtures/game.json'
+    game.solve # => true
+    puts game
+
+TODO
+----
+
+* Add more puzzles for testing
+* Prettier output, [Rubyquiz](http://rubyquiz.com/quiz43.html) format
+* Handle unsolvable puzzles
+* Handle puzzles with multiple solutions (more common with *hard* puzzles)
+* CLI:
+    * Read json: sudoku [json filename]
+    * Read [Rubyquiz](http://rubyquiz.com/quiz43.html) format: sudoku <stdin>
+* Image processing? (ie read the puzzle from an image)
+    * [Tesseract OCR](http://code.google.com/p/tesseract-ocr/)
+* Fix stack level error when inspecting objects (recursive relationships make an infinite loop)
---
+++ lib/sudoku.rb
@@ -0,0 +1,161 @@
+require 'rubygems'
+require 'json'
+
+module Sudoku
+
+  VERSION = "0.0.0" unless defined?(VERSION)
+
+  # autoload :Game,    "sudoku/game"
+  # autoload :Answer,  "sudoku/answer"
+  # autoload :Set,     "sudoku/set"
+
+  class Game
+    attr_accessor :answers, :sets
+
+    def initialize
+      @answers = (0..80).map{|index| Answer.new(index) }
+      @sets = []
+
+      rows = @answers.dup
+      9.times{ set rows.shift(9), 'row' }
+
+      9.times{ |n| column @answers.dup, n }
+
+      [0,3,6,27,30,33,54,57,60].each do |n|
+         box @answers.dup, n
+      end
+    end
+
+    def load_gamefile(file)
+      self.defaults JSON.parse(File.read(file)).flatten
+    end
+
+    def defaults(defaults)
+      @answers.each_with_index do |answer, id|
+        if value = defaults[id] and !value.zero?
+          answer.default =  value
+        end
+      end
+    end
+
+    def set(answers, type)
+      @sets << Set.new(answers, type)
+    end
+
+    def column(answers, offset=0)
+      box(answers, offset, 1, 8, 'column')
+    end
+
+    def box(answers, offset=0, take=3, leave=6, type='box')
+      answers.shift(offset)
+      set = []
+      until set.size == 9
+        set += answers.shift(take)
+        answers.shift(leave)
+      end
+      set(set, type)
+    end
+
+    def unsolved
+      @answers.select &:unsolved?
+    end
+
+    def solve(count = unsolved.size)
+      @sets.each{|set| set.solve }
+      if count != (new_count = unsolved.size)
+        solve new_count
+      end
+      unsolved.size.zero?
+    end
+
+    def inspect
+      '#<%s:0x%s>' % [self.class, object_id.to_s(16)]
+    end
+
+    def to_s
+      @sets.select{|set| set.type == 'row' }.map{|set| set.answers.map(&:to_s).join(' ') }.join("\n")
+    end
+  end
+
+  class Answer
+    attr_accessor :value, :guesses, :default, :index, :sets
+
+    def initialize(index)
+      @index = index
+      @sets = []
+      @guesses = (1..9).to_a
+    end
+
+    def default=(value)
+      unless value.nil?
+        solve value
+      end
+      @value = @default = value
+    end
+
+    def solve(value)
+      @guesses.clear
+      @value = value
+      remove_guesses value
+    end
+
+    def remove_guesses(value)
+      @sets.each{|set| set.remove_guesses(value) }
+    end
+
+    def remove_guess(value)
+      raise "#{index} only has one guess #{guesses.inspect} can't remove #{value}" if guesses == [value]
+      guesses.delete(value)
+    end
+
+    def unsolved?
+      @value.nil?
+    end
+
+    def to_s
+      '%02s' % (value || '_')
+    end
+  end
+
+  class Set
+    attr_accessor :answers, :type
+
+    def initialize(answers, type)
+      @answers, @type = answers, type
+      @answers.each do |answer|
+        answer.sets << self
+      end
+    end
+
+    def unsolved
+      @answers.select &:unsolved?
+    end
+
+    def remove_guesses(value)
+      @answers.each{|answer| answer.remove_guess(value) }
+    end
+
+    def solve
+      only_guess
+      only_match
+    end
+
+    def only_guess
+      unsolved.select{|s| s.guesses.size == 1 }.map{|s| s.solve s.guesses.first  }
+    end
+
+    def only_match
+      unsolved.inject({}) do |matches,s|
+        s.guesses.each do |guess|
+          matches[guess] ||= []
+          matches[guess] << s
+        end
+        matches
+      end.each do |guess, set|
+        if set.size == 1
+          set.first.solve guess
+        end
+      end
+    end
+  end
+end
